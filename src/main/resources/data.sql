84b8f7650d3ebd4cf4b6698be1279d40755a6394
Tomasz Stachura <203996@edu.p.lodz.pl>
2020-01-15 22:13:51 +0100

    Code refactoring

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -80,7 +80,7 @@ INSERT INTO public.department (id, deleted, name, version) VALUES ('9b102026-364
 
 --//---- USER ----//--
 INSERT INTO public.user_ (id, account_expired, account_locked, credentials_expired, enabled, password, user_name, version, userdata_id) VALUES ('14e2df64-2d6a-11ea-978f-2e728ce88125', false, false, false, true, '$2a$08$qvrzQZ7jJ7oy2p/msL4M0.l83Cd0jNsX6AJUitbgRXGzge4j035ha', 'admin', 0, '65efc27a-1f21-11ea-978f-2e728ce88125');
-INSERT INTO public.user_ (id, account_expired, account_locked, credentials_expired, enabled, password, user_name, version, userdata_id) VALUES ('26592f5a-2d6a-11ea-978f-2e728ce88125', false, false, false, true, '$2a$08$KjqKtpwHqXLJWo2XEP5u6uYJ2gtkKPNMgKxAnlG9pJnbzl4QQWIQ6', 'user', 0, 'd9700e86-2d94-11ea-978f-2e728ce88125');
+INSERT INTO public.user_ (id, account_expired, account_locked, credentials_expired, enabled, password, user_name, version, userdata_id) VALUES ('26592f5a-2d6a-11ea-978f-2e728ce88125', false, false, false, true, '$2a$08$EBkM9ioW5YS0izjrngkLTOcGJARIU2X5D3Tu8UstUWGQyQ4HGV5fa', 'user', 0, 'd9700e86-2d94-11ea-978f-2e728ce88125');
 
 --//---- USER ROLE ----//--
 INSERT INTO USER_ROLE(ID,NAME,ACTIVE) VALUES ('75e20624-1f22-11ea-a32c-2e728ce88125','ADMIN',TRUE );
